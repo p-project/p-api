444084c5203903bfa116aee6eb0c6e37bcd14494
mike1796 <mickael_brunel@hotmail.fr>
2017-07-26 15:08:24 +0200

    Forgot one comment to remove Remove warning for Behatch\Extension

--- web/app.php
+++ web/app.php
@@ -15,7 +15,6 @@ $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
 
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
-//Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
